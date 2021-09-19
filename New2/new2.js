6ac7dbf2458155b032538de2418813ca539a3731
MeowHoa <thebest270603@gmail.com>
2021-09-19 10:01:01 +0700

    hai

--- New2/new2.js
+++ New2/new2.js
@@ -1,2 +1,8 @@
-console.log('ngoan vua thoi')
-console.log('hu vua thoi')
+
+let a= `i'm hai`
+
+console.log(a )
+console.log('a'+4+5)
+console.log(4+5+'a')
+let txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
